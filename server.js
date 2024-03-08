553a45ef70aa34f995641404258fbadfd192ef23
Raymond Joseph <rjpgalupe@gmail.com>
2024-03-08 17:47:42 +0800

    CORS

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/uploads', express.static('uploads'));
 
 app.use((req, res, next) => { 
     res.header("Access-Control-Allow-Origin",  
-               "http://localhost:4200"); 
+               "http://localhost:4200", "https://soc-jms.vercel.app"); 
     res.header("Access-Control-Allow-Headers",  
                "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
